4fa59743deb80fc21e1e4fbd7ae2a4de181032f1
season93 <season.gane@gmail.com>
2017-11-01 19:53:48 +0530

    MySample
    01/11/2017

---
+++ DriverDetails.java
@@ -0,0 +1,119 @@
+package models;
+
+public class DriverDetails {
+	
+	String driver_name;
+	int contact_number;
+	String experience;
+	String currently_driving;
+	String bank_name;
+	String account_number;
+	String ifsc_code;
+	String address;
+	String vehicle_number;
+	String vehicle_make;
+	String license_number;
+	String insurance;
+	String preferred_area;
+	String email;
+	String city;
+	String enrolled_date;
+	public String getDriver_name() {
+		return driver_name;
+	}
+	public void setDriver_name(String driver_name) {
+		this.driver_name = driver_name;
+	}
+	public int getContact_number() {
+		return contact_number;
+	}
+	public void setContact_number(int contact_number) {
+		this.contact_number = contact_number;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	public String getCurrently_driving() {
+		return currently_driving;
+	}
+	public void setCurrently_driving(String currently_driving) {
+		this.currently_driving = currently_driving;
+	}
+	public String getBank_name() {
+		return bank_name;
+	}
+	public void setBank_name(String bank_name) {
+		this.bank_name = bank_name;
+	}
+	public String getAccount_number() {
+		return account_number;
+	}
+	public void setAccount_number(String account_number) {
+		this.account_number = account_number;
+	}
+	public String getIfsc_code() {
+		return ifsc_code;
+	}
+	public void setIfsc_code(String ifsc_code) {
+		this.ifsc_code = ifsc_code;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getVehicle_number() {
+		return vehicle_number;
+	}
+	public void setVehicle_number(String vehicle_number) {
+		this.vehicle_number = vehicle_number;
+	}
+	public String getVehicle_make() {
+		return vehicle_make;
+	}
+	public void setVehicle_make(String vehicle_make) {
+		this.vehicle_make = vehicle_make;
+	}
+	public String getLicense_number() {
+		return license_number;
+	}
+	public void setLicense_number(String license_number) {
+		this.license_number = license_number;
+	}
+	public String getInsurance() {
+		return insurance;
+	}
+	public void setInsurance(String insurance) {
+		this.insurance = insurance;
+	}
+	public String getPreferred_area() {
+		return preferred_area;
+	}
+	public void setPreferred_area(String preferred_area) {
+		this.preferred_area = preferred_area;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEnrolled_date() {
+		return enrolled_date;
+	}
+	public void setEnrolled_date(String enrolled_date) {
+		this.enrolled_date = enrolled_date;
+	}
+
+
+}
